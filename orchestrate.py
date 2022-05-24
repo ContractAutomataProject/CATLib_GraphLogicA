c9a128a00c0b4cd00f240e7ba6ec75554368288b
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-05-24 08:02:11 +0200

    added experiment2

--- orchestrate.py
+++ orchestrate.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+#!/bin/env python3
+
+# %%
+
 # Setup
 import voxlogica
 import os
@@ -6,6 +9,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import json
 import os.path
+from joblib import Parallel, delayed
+import multiprocessing
 
 baseimage = './src/test/java/io/github/contractautomata/maze/resources/maze3.png'
 datadir = './src/test/java/io/github/contractautomata/maze/resources/twoagentsimages/png'
@@ -13,21 +18,18 @@ images = [fname for fname in os.listdir(datadir) if fname.endswith(".png")]
 tmpdir = "./tmp"
 cache = "./cache.json"
 # Result computation and auxiliary function "view"
-def run_analysis(specification,image):
-    print(f'Analysing {image}... ', end='')
-    res = voxlogica.run_voxlogica(specification(image))
-    print('done')
-    return res
-
-
 def compute(specification,start=0,end=len(images)-1):
-    voxlogica_output = [
-        {"filename": image, "output": run_analysis(specification,image)} for image in images[start:end+1]]
+    num_cores = 1 #multiprocessing.cpu_count()
 
-    # %%
+    def processInput(image):
+        print(f'Analysing {image}... ', end='')
+        x = {"filename": image, "output": voxlogica.run_voxlogica(specification(image))}
+        print('done')
+        return x
+
+    voxlogica_output = Parallel(n_jobs=num_cores)(delayed(processInput)(image) for image in images[start:end+1])
+
     return voxlogica.simplify_results(voxlogica_output)
-# %%
-
 
 def view(result,rows = 5,cols = 5,sizex = 20,sizey = 20):
     fname = result["filename"]
@@ -50,7 +52,9 @@ def view(result,rows = 5,cols = 5,sizex = 20,sizey = 20):
             plt.imshow(img,cmap="Greys_r")
             i = i+1            
     plt.show()     
-    print(toprint)           
+    print(toprint)  
+
+# %%  
 # Specification and analysis
 def vlsave(image,var):
     return f'save "{tmpdir}/{image}_{var}.png" {var}'
@@ -86,25 +90,40 @@ let mrGreen = (r =. 0) & (b =. 0) & (g =. 255)
 
 let mrX = mrRed | mrGreen
 
-let wrong = exists(mrX & wall)
+let initial1 = exists(mrRed & ((x =. 1) & (y =. 1))) .&. exists(mrGreen & ((x =. 1) & (y =. 2)))
+let initial2 = exists(mrRed & ((x =. 6) & (y =. 3))) .&. exists(mrGreen & ((x =. 1) & (y =. 4)))
+let wrong = exists(mrX & wall) .|. (!. (exists(mrRed) .&. exists(mrGreen)))
 let exit = (x =. 9) & (y >. 2) & (y <. 9)
 let pathToExit = (floor ~> exit)
 let canExit(mr) = forallin(mr,pathToExit)
 let sameRoom = forallin(mrGreen,(mrGreen|floor) ~> mrRed)
 
-let greenEscapes = (!.wrong) .&. canExit(mrGreen) .&. (!.(canExit(mrRed)))
+let greenFlees = (!.wrong) .&. canExit(mrGreen) .&. (!.(canExit(mrRed)))
 let nearby = exists(mrRed & (N N mrGreen))
 
+let forbidden1 = greenFlees .|. wrong
+let forbidden2 = forbidden1 .|. (!. nearby)
+
+let final = exists(mrRed & exit) .&. exists(mrGreen & exit)
+
+{vlprint("initial1")}
+{vlprint("initial2")}
+{vlprint("forbidden1")}
+{vlprint("forbidden2")}
 {vlprint("canExit(mrGreen)")}
 {vlprint("wrong")}
 {vlprint("sameRoom")}
-{vlprint("greenEscapes")}
+{vlprint("greenFlees")}
 {vlprint("nearby")}
+
+
 {vlsave(image,"door")}
 {vlsave(image,"exit")}
 {vlsave(image,"mrRed")}
 {vlsave(image,"mrGreen")}
 {vlsave(image,"pathToExit")}
+
+
 
 
 '''
@@ -117,13 +136,4 @@ else:
     with open(cache, 'w') as f:
         json.dump(items, f)    
 
-ok = [item for item in items if not item["results"]["wrong"] ]
-len(ok)
-escape = [item for item in items if item["results"]["greenEscapes"] ]
-len(escape)
-
-# %%
-
-ok = [item for item in items if not item["results"]["wrong"] ]
-escape = [item for item in ok if item["results"]["greenEscapes"] ]
 # %%
